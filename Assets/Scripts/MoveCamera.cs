ee29f476044e13b4ea4f08042fa7b2301359d0bc
Matthew Brooks <Matthew Brooks>
2016-05-03 11:00:59 +0100

    first commit

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,70 @@
+﻿// Credit to damien_oconnell from http://forum.unity3d.com/threads/39513-Click-drag-camera-movement
+// for using the mouse displacement for calculating the amount of camera movement and panning code.
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+	// User Controlled Change
+	public float MoveRate;
+	public float TurnRate = 90f;
+
+	// Update Change Rate
+	public float InternalTurnRate = 80f;
+	public float InternalMoveRate = 5f;
+
+	// Camera Rotation
+	public float Yaw = 0;
+	public float Pitch = 0;
+	// Enforced View Limits
+	public float PitchMax = 80;
+	public float PitchMin = -80;
+
+	private float hAxis;
+    private float vAxis;
+    private float adAxis;
+    private float auAxis;
+	private float htAxis;
+	private float vtAxis;
+
+	// Target Position
+	private Quaternion tRotation;
+	private Vector3 tPosition;
+
+	void Update()
+    {
+        htAxis = Input.GetAxis("HorizontalCam");
+        vtAxis = -Input.GetAxis("VerticalCam");
+        adAxis = -Input.GetAxis("AltitudeDown");
+        auAxis = Input.GetAxis("AltitudeUp");
+		hAxis = Input.GetAxis("Horizontal");
+		vAxis = Input.GetAxis("Vertical");
+
+		float aAxis = adAxis + auAxis;
+
+		// Compute Yaw/Pitch for the current update
+		Yaw += htAxis * TurnRate * Time.deltaTime;
+		Pitch += vtAxis * TurnRate * Time.deltaTime;
+
+		// Compute Position Change
+		tPosition = transform.position + MoveRate * Time.deltaTime * (
+			transform.right * hAxis +
+			transform.forward * vAxis +
+			Vector3.up * aAxis);
+
+		// Adjust Pitch/Yaw and compute Quaternion
+		Pitch = Mathf.Clamp(Pitch, PitchMin, PitchMax);
+		//		Yaw = Utils.LimitAngles(Yaw);
+		tRotation = Quaternion.Euler(-Pitch, Yaw, 0f);
+
+		// Move/Rotate Camera Rig to computed target position/rotation
+		//		transform.rotation = Quaternion.RotateTowards(transform.rotation, tRotation, InternalTurnRate * Time.deltaTime);
+		//		transform.position = Vector3.Lerp(transform.position, tPosition, InternalMoveRate * Time.deltaTime);
+
+		transform.rotation = tRotation;
+		transform.position = tPosition;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed; //shows up in Inspector as public property
+    public Text countText;
+    public Text winText;
+	public AudioClip pickupSound;
+
+    private Rigidbody rb;
+    private int count;
+	private AudioSource source;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+	void Awake()
+	{
+		source = GetComponent<AudioSource>();
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("HorizontalDPAD");
+        float moveVertical = Input.GetAxis("VerticalDPAD");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+			source.PlayOneShot(pickupSound, 1.0f);
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
